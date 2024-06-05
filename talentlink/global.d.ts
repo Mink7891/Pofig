37903045a6049a6dfc3eeeb1d90086eb31782a07
BUREVOY <mamakolama@gmail.com>
2024-06-05 12:05:24 +0300

    убрал ошибки в редакторе

---
+++ talentlink/global.d.ts
@@ -0,0 +1,8 @@
+// global.d.ts
+import React from 'react';
+
+declare module 'react' {
+  interface ImgHTMLAttributes<T> {
+    className?: string; // Добавляем опциональное свойство className к ImgHTMLAttributes
+  }
+}
