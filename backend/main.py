b9c1a23e14119408e2a78125d91d24a93112bfb9
Pr0grammer-learner <tima.privalov@bk.ru>
2024-06-14 12:09:31 +0300

    blablabla

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def get_home():
-    return {"data": "Hello world!"}
+class SCVAdd(BaseModel):
+   
+   appointment: str
+   specialization: str 
+
+# class SCV(SCVAdd):
+#    id: int
+#    model_config = ConfigDict(from_attributes=True)
+
+
+@app.get("/CV")
+def get_CV():
+    cv = SCVAdd(appointment="Разработчик", specialization="Backend-программист")
+    return {"data": cv}
